1e187e457d2b96f844f88bee055fb460c9ccdc12
Y.V <5196666qwe@gmail.com>
2017-09-19 21:12:12 +0800

    自定义error

--- utils/commonutils.js
+++ utils/commonutils.js
@@ -7,8 +7,7 @@ module.exports = {
     loadGB2312,
     load,
     encode,
-    requestPromise,
-    getChineseOrCDB
+    requestPromise
 };
 
 function requestPromise(options) {
@@ -80,28 +79,3 @@ function encode(str, charset) {
 }
 
 
-function getChineseOrCDB(str) {
-    var isChinese = /[\u4e00-\u9fa5]/g.test(str);
-    //全角
-    var isDBC = /[\uff00-\uffff]/g.test(str);
-    //半角
-    //var isCDB= /[\u0000-\u00ff]/g.test(str)
-    return isChinese ? str : isDBC ? ToCDB(str) : str;
-}
-
-function ToCDB(str) {
-    var tmp = "";
-    for (var i = 0; i < str.length; i++) {
-        if (str.charCodeAt(i) == 12288) {
-            tmp += String.fromCharCode(str.charCodeAt(i) - 12256);
-            continue;
-        }
-        if (str.charCodeAt(i) > 65280 && str.charCodeAt(i) < 65375) {
-            tmp += String.fromCharCode(str.charCodeAt(i) - 65248);
-        }
-        else {
-            tmp += String.fromCharCode(str.charCodeAt(i));
-        }
-    }
-    return tmp
-}
--- utils/readline.js
+++ utils/readline.js
@@ -1,10 +1,7 @@
 var readline = require('readline');
-var getChineseOrCDB = require('./commonutils').getChineseOrCDB;
-
 var prompt = `\n请选择你想要获取的信息?\n\n 1. 新电影 2. 音乐 3. 小说 4. 番剧 5. wallhaven \n\n > `
 
 module.exports.Readline = new Readline(prompt);
-
 
 function Readline(prompt = `提示`) {
     var rl = readline.createInterface({
@@ -28,3 +25,28 @@ function Readline(prompt = `提示`) {
 }
 
 
+function getChineseOrCDB(str) {
+    var isChinese = /[\u4e00-\u9fa5]/g.test(str);
+    //全角
+    var isDBC = /[\uff00-\uffff]/g.test(str);
+    //半角
+    //var isCDB= /[\u0000-\u00ff]/g.test(str)
+    return isChinese ? str : isDBC ? ToCDB(str) : str;
+}
+
+function ToCDB(str) {
+    var tmp = "";
+    for (var i = 0; i < str.length; i++) {
+        if (str.charCodeAt(i) == 12288) {
+            tmp += String.fromCharCode(str.charCodeAt(i) - 12256);
+            continue;
+        }
+        if (str.charCodeAt(i) > 65280 && str.charCodeAt(i) < 65375) {
+            tmp += String.fromCharCode(str.charCodeAt(i) - 65248);
+        }
+        else {
+            tmp += String.fromCharCode(str.charCodeAt(i));
+        }
+    }
+    return tmp
+}
